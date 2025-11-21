f3f668223984afd09d26f56b459c9d70fd52cf62
dannyfadli <danyfdly95@gmail.com>
2025-11-21 11:14:10 +0700

    menggati semua kode di formullir

---
+++ app/src/main/java/com/example/myarsitektur_mvvm/model/Data/Siswa.kt
@@ -0,0 +1,8 @@
+package com.example.myarsitektur_mvvm.model.Data
+
+// Perhatikan: properti dipindahkan ke dalam tanda kurung ()
+data class Siswa(
+    val nama : String = "",
+    val gender : String = "",
+    val alamat : String = ""
+)
--- app/src/main/java/com/example/myarsitektur_mvvm/view/Formulir.kt
+++ app/src/main/java/com/example/myarsitektur_mvvm/view/Formulir.kt
@@ -9,10 +9,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.Button
 import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
@@ -20,6 +22,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -27,13 +33,25 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.myarsitektur_mvvm.R
+import androidx.compose.runtime.getValue // Penting untuk 'by'
+import androidx.compose.runtime.setValue // Penting untuk 'by'
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormIsian(
-    jenisK:List<String> = listOf("Laki-Laki","Perempuan"),
-    OnSubmitBtnClick : () -> Unit
+//    jenisK:List<String> = listOf("Laki-Laki","Perempuan"),
+//    OnSubmitBtnClick : () -> Unit
+
+    JenisJK: List<String>,
+    onSubmitButtonClicked:(MutableList<String>) -> Unit
 ){
+    var txtNama by rememberSaveable{mutableStateOf("")}
+    var txtALamat by remember { mutableStateOf("") }
+    var txtGender by remember { mutableStateOf("") }
+
+    val LisData : MutableList<String> = mutableListOf(txtNama,txtGender,txtALamat)
+
+
     Scaffold (modifier= Modifier,
         {
             TopAppBar(
@@ -46,26 +64,41 @@ fun FormIsian(
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally){
             OutlinedTextField(
-                value = "",
+                value = txtNama,
                 singleLine = true,
-                modifier = Modifier
-                    .padding(top = 20.dp)
-                    .width(250.dp),
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.width(250.dp).padding(top = 20.dp),
                 label = {Text(text = "Nama Lengkap")},
-                onValueChange = {},
+                onValueChange = {
+                    txtNama = it
+                }
             )
             HorizontalDivider(modifier = Modifier
                 .padding(20.dp)
                 .width(250.dp), thickness = Thickness, color = Color.Red)
             Row {
-                jenisK.forEach {
-                        item ->
-                    Row (verticalAlignment = Alignment.CenterVertically){
+                JenisJK.forEach { item ->
+                    Row (
+                        modifier = Modifier.selectable(
+                            selected = txtGender == item,
+                            onClick = {
+                                txtGender = item
+                            }
+                        ).padding(end = 16.dp), // Tambahkan padding agar tidak terlalu rapat
+
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center
+                    ) {
                         RadioButton(
-                            selected = false,
-                            onClick = {item}
+                            selected = txtGender == item,
+                            // OnClick RadioButton juga harus mengupdate state
+                            onClick = {
+                                txtGender = item
+                            }
                         )
-                        Text(text = item)
+                        Text(
+                            text = item,
+                        )
                     }
                 }
             }
@@ -76,18 +109,20 @@ fun FormIsian(
                 color = Color.Red
             )
             OutlinedTextField(
-                value = "",
+                value = txtALamat,
                 singleLine = true,
-                modifier = Modifier
-                    .width(250.dp),
-                label = {Text(text = "Alamat")},
-                onValueChange = {},
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.width(250.dp),
+                label = {Text(text = "ALamat Lengkap")},
+                onValueChange = {
+                    txtALamat = it
+                }
             )
             Spacer(modifier = Modifier.height(30.dp))
             Button(
                 modifier = Modifier.fillMaxWidth(1f)
                     .padding(all = 25.dp),
-                onClick = OnSubmitBtnClick
+                onClick = {onSubmitButtonClicked(LisData)}
             ) {
                 Text(stringResource(R.string.submit))
             }
