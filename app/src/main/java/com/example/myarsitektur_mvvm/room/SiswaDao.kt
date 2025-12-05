fefdfef41de84cb4f8b8f159f21997a22003be1f
dannyfadli <danyfdly95@gmail.com>
2025-12-05 09:10:04 +0700

    Add query to retrieve Siswa entity by ID

--- app/src/main/java/com/example/myarsitektur_mvvm/room/SiswaDao.kt
+++ app/src/main/java/com/example/myarsitektur_mvvm/room/SiswaDao.kt
@@ -16,4 +16,7 @@ interface SiswaDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa: Siswa)
 
+    @Query("SELECT * from tblSiswa WHERE id = :id")
+    fun getSiswa(id: int):Flow<Siswa>
+
 }
