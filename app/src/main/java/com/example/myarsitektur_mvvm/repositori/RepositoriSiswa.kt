811eb65108b213e3709391088d2f5c56e38d6a9a
dannyfadli <danyfdly95@gmail.com>
2025-12-05 09:18:22 +0700

    Menghapus Siswa Delete

--- app/src/main/java/com/example/myarsitektur_mvvm/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/myarsitektur_mvvm/repositori/RepositoriSiswa.kt
@@ -20,7 +20,7 @@ class OfflineRepositoriSiswa(
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
 
     override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
-//    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
 //    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
 
 }
